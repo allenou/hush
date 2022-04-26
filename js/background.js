1ee04adac61d874a1d5f59241e5e1f2ea8b2f801
allenou <jskindler@outlook.com>
2022-04-27 07:52:34 +0800

    update

--- js/background.js
+++ js/background.js
@@ -16,4 +16,29 @@ chrome.runtime.onMessage.addListener(async (message, sender, sendResponse) => {
       "我是 background，我已收到你的消息：" + JSON.stringify(message)
     );
   }
+  if(message.export){
+    exportExcel();
+  }
 });
+
+/**
+ * @description 导出表格
+ */
+async function exportExcel() {
+  const data = await chrome.storage.local.get(["SHOPS"]);
+  if (!data || data.SHOPS.length === 0) {
+    alert("没有数据可导出");
+    return;
+  }
+  const worksheet = XLSX.utils.json_to_sheet(data.SHOPS);
+
+  const workbook = XLSX.utils.book_new();
+
+  XLSX.utils.book_append_sheet(workbook, worksheet, "Sheet1");
+
+  XLSX.writeFile(workbook, "大众点评店铺信息表.xlsx");
+
+  chrome.storage.local.set({
+    SHOPS: [],
+  });
+}
